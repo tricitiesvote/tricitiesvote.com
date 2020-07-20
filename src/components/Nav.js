ef284b1f924c2ec6c918fb0910d8197868ecaf50
Adam Avenir <adam@andyet.com>
2020-07-19 22:11:34 -0700

    more nav cleanup

---
+++ src/components/Nav.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { graphql, StaticQuery, Link } from 'gatsby';
+
+const Nav = () => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          allGuidesJson(
+            filter: {
+              electionyear: {eq: "2020"}, 
+              type: {eq: "primary"}
+            }
+          ) {
+            edges {
+              node {
+                fields {
+                  slug
+                }
+                electionyear
+                type
+                region
+              }
+            }
+          }
+        }
+      `}
+      render={data => (
+        <nav>
+          <Link to="/">Home</Link>
+          {data.allGuidesJson.edges.map(guide => (
+            <Link to={'/' + guide.node.fields.slug}>{guide.node.region} guide</Link>
+          ))}
+        </nav>
+      )}
+    />
+  );
+};
+
+export default Nav;
