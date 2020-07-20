9611e195a6ad2f2aeeeaefdff4972a4176df4b8e
Adam Avenir <adam@andyet.com>
2020-07-19 21:56:50 -0700

    only show race if theres stuff in it

--- src/components/RaceList.js
+++ src/components/RaceList.js
@@ -4,12 +4,29 @@ import Race from "./Race";
 
 const RaceList = props => {
   const { data } = props;
+  const races = []
+
+  // allCandidates.forEach((candidate, index) => {
+  //   createPage({
+  //     path: `/${candidate.node.fields.slug}/`,
+  //     component: path.resolve('./src/templates/CandidatePage.js'),
+  //     context: {
+  //       slug: candidate.node.fields.slug,
+  //     },
+  //   })
+  // })
+
+  data.forEach((item) => {
+    if (item.candidates) {
+      races.push(item);
+    }
+  })
 
   // is this a list of races or a list of candidates?
 
   return (
     <div className="races-collection" key={data.uuid}>
-      {data.map(race => (
+      {races.map(race => (
         <section className="race" key={race.uuid}>
           <Link to={'/' + race.fields.slug}>
             <h2>{race.office.title}</h2>
