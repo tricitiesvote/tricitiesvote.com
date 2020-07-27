d07773cefd50f34d90c10ccd057456474e8c7ed6
Adam Avenir <adam@andyet.com>
2020-07-26 23:13:37 -0700

    cleanup

--- data/cheerio/index.js
+++
@@ -1,8 +0,0 @@
-const request = require('request');
-const cheerio = require('cheerio');
-
-request('https://voter.votewa.gov/genericvoterguide.aspx?e=865#/candidates/57373/66947', function (error, response, html) {
-  if (!error && response.statusCode == 200) {
-    console.log(html);
-  }
-});
