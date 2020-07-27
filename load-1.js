dfb5b7bc635b0dc603d1968de3d651bbbd970569
Adam Avenir <adam@andyet.com>
2020-07-27 13:40:13 -0700

    update donor data load scripts

--- load-1.js
+++ load-1.js
@@ -26,30 +26,6 @@ consumer.query()
     .on('success', function(rows) { 
       rows.forEach((row, index) => {
         // console.log(row);
-        let item = {
-          // create donation record
-          'candidate_filer_id': row.filer_id,
-          'pdc_url': `https://www.pdc.wa.gov/browse/campaign-explorer/candidate?filer_id=${row.filer_id}&election_year=2020`,
-          'candidate_fullname': _.startCase(_.lowerCase(row.filer_name)),
-          'candidate_firstname': _.startCase(_.lowerCase(row.first_name)),
-          'candidate_lastname': _.startCase(_.lowerCase(row.last_name)),
-          'office': _.startCase(_.lowerCase(row.office)),
-          'district': parseInt(row.legislative_district, 10),
-          'position': parseInt(row.position, 10),
-          'county': _.startCase(_.lowerCase(row.jurisdiction_county)),
-          'party': _.startCase(_.lowerCase(row.party)),
-          'election_year': row.election_year,
-          'donor_type': row.code,
-          'donation_type': row.cash_or_in_kind,
-          'donation_amount': row.amount,
-          'donation_date': row.receipt_date,
-          'donation_description': row.description,
-          'donor_name': _.startCase(_.lowerCase(row.contributor_name)),
-          'donor_city': _.startCase(_.lowerCase(row.contributor_city)),
-          'election_type': row.primary_general,
-          'report_url': row.url.url,
-        }
-        dataSet.push(item)
 
         // check to see if we already have the candidate
         if (!_.includes(filer_ids, row.filer_id)) {
@@ -88,11 +64,6 @@ consumer.query()
         }
 
       })
-
-      // write  donation data
-      let data = JSON.stringify(dataSet, null, 2);
-      fs.writeFileSync('./data/donors/donors.json', data);
-      console.log(dataSet.length, 'items written to data/donors/donations.json');
 
       // write candidate data
       let candidateData = JSON.stringify(candidates, null, 2);
