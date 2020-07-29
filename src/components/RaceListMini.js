933a0ab40fb3762106ceb6c787bc382fd796a4d6
Adam Avenir <adam@andyet.com>
2020-07-29 13:42:39 -0700

    fix mobile layout issue, i think

--- src/components/RaceListMini.js
+++ src/components/RaceListMini.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import _ from 'lodash';
 import CandidateMini from './CandidateMini';
 
 const RaceListMini = props => {
   const { data } = props;
 
+  const races = [];
+
+  data.forEach(race => {
+    // eslint-disable-next-line prettier/prettier
+    if (!_.find(races, race)) {
+      races.push(race);
+    }
+  });
+
+  // (!_.find(races { office}, { office: { title: race.office.title } })
+
   return (
     <div className="races-collection" key={data.id}>
-      {data.map(race => (
-        <section className="race" key={race.uuid}>
+      <pre>{/* <code>{JSON.stringify(races[1], null, 2)}</code> */}</pre>
+      {races.map(race => (
+        <section className="race" key={race.id}>
           <Link to={`/${race.fields.slug}`}>
             <h2>{race.office.title}</h2>
           </Link>
