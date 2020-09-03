e5b5db79d48b5ad6a7a6a640d286efd7fe391fe9
Adam Avenir <adam@andyet.com>
2020-09-03 03:37:26 -0700

    wip

--- src/components/Guide.js
+++ src/components/Guide.js
@@ -1,16 +1,27 @@
 import React from 'react';
 // import { Link } from "gatsby"
+import _ from 'lodash';
 import RaceList from './RaceList';
 import RaceListMini from './RaceListMini';
 
 const Guide = props => {
   const { data, mini } = props;
   const { races } = data.node;
+  // const flattened = _.flatten(races);
+  const flattened = _.flattenDepth(races, 1);
+  const dedupedRaces = _.uniqBy(flattened, 'fields.slug');
 
   return (
     <div className="guide" key={data.uuid}>
-      {races.region ? <h1>{races.region}</h1> : ''}
-      {mini ? <RaceListMini data={races} /> : <RaceList data={races} />}
+      <pre>
+        <code>{JSON.stringify(dedupedRaces, null, 2)}</code>
+      </pre>
+      {dedupedRaces.region ? <h1>{dedupedRaces.region}</h1> : ''}
+      {mini ? (
+        <RaceListMini data={dedupedRaces} />
+      ) : (
+        <RaceList data={dedupedRaces} />
+      )}
     </div>
   );
 };
