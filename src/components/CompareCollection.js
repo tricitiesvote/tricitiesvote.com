24bc9a4a6701ec784a525711101f81ec10d1fabb
Adam Avenir <adam@andyet.com>
2020-10-19 00:35:53 +0000

    delete some unnecessary files

--- src/components/CompareCollection.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import CompareStatements from './CompareStatements';
-
-const CompareCollection = props => {
-  return (
-    // map CompareStatements
-    <CompareStatements />
-  );
-};
