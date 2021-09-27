1cb662406c88f0297e9666cc9c91a083c9a7096f
Adam Avenir <adam@andyet.com>
2021-09-27 16:01:40 -0700

    cleanup

--- src/components/CandidateEndorsements.js
+++ src/components/CandidateEndorsements.js
@@ -2,15 +2,11 @@ import React from 'react';
 
 const CandidateEndorsements = ({ endorsements }) => {
   return (
-    <>
+    <div className="endorsements-summary">
       {endorsements ? (
         <>
           <ul className="recs">
             <h3>Endorsements and letters</h3>
-            <p>
-              <em>Written</em> letters describing why people support or oppose
-              the candidate.
-            </p>
             {endorsements.map(e => (
               <>
                 {e.forAgainst === 'for' ? (
@@ -37,7 +33,7 @@ const CandidateEndorsements = ({ endorsements }) => {
           <p>No letters of support or opposition listed yet.</p>
         </>
       )}
-    </>
+    </div>
   );
 };
 
