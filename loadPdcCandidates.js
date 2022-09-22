72d18c319e7042f91918c2a2632d5bd4d088c9e6
Adam Avenir <adamavenir@hey.com>
2022-09-21 22:22:26 -0700

    using config method

--- loadPdcCandidates.js
+++ loadPdcCandidates.js
@@ -5,6 +5,8 @@ const NAMES = require('./load-config-names.json');
 const CONFIG = require('./load-config-election.json');
 
 module.exports = new Promise((resolve, reject) => {
+  
+  // go to this address to interact with this data in the browser:
   // https://data.wa.gov/Politics/Contributions-to-Candidates-and-Political-Committe/kv7h-kjye/data
   const consumer = new soda.Consumer('data.wa.gov');
 
@@ -14,11 +16,12 @@ module.exports = new Promise((resolve, reject) => {
 
   consumer
     .query()
-    .withDataset('7qr9-q2c9') // campaign finance reports
+    .withDataset(CONFIG.pdcDataset) // campaign finance reports
     .limit(10000)
     .where(
       `
-      election_year = '${CONFIG.year}' AND (
+      election_year = '${CONFIG.year}' AND
+      type = 'Candidate' AND (
         jurisdiction = 'CITY OF RICHLAND' OR
         jurisdiction = 'CITY OF KENNEWICK' OR
         jurisdiction = 'CITY OF WEST RICHLAND' OR
@@ -36,15 +39,20 @@ module.exports = new Promise((resolve, reject) => {
     .getRows()
     .on('success', function(rows) {
       for (const row of rows) {
-        // console.log(row);
 
         // check to see if we already have the candidate
         if (!_.includes(filer_ids, row.filer_id)) {
+          
+          // fallback to row.filer_name for candidate name
           let candidateName = row.filer_name;
-          if (_.find(NAMES, {'pdcId': row.filer_id}) && row.type === "Candidate") {
-            candidateName = _.find(NAMES, {'pdcId': row.filer_id})
-          } else if (row.type === "Candidate") {
-            // console.log(`{\n  "formattedName": "",\n  "pdcId": "${row.filer_id}",\n  "altNames": [\n    "${row.filer_name}"\n  ]\n},`);
+          
+          // if ID has a match in load-config-names.json, use that name
+          if (_.find(NAMES, {'pdcId': row.filer_id}) ) {
+            candidateName = _.find(NAMES, {'pdcId': row.filer_id}).formattedName;
+            
+          // or else grab the candidate name and PDC Id
+          // so we can output that for updating load-config-names.json
+          } else {
             const thisCandidate = {
               formattedName: "",
               pdcId: row.filer_id,
@@ -53,7 +61,7 @@ module.exports = new Promise((resolve, reject) => {
             newCandidates.push(thisCandidate)
           }
           const candidate = {
-            pdc_url: `https://www.pdc.wa.gov/browse/campaign-explorer/candidate?filer_id=${row.filer_id}&election_year=2021`,
+            pdc_url: `https://www.pdc.wa.gov/browse/campaign-explorer/candidate?filer_id=${row.filer_id}&election_year=${CONFIG.year}`,
             candidate_filer_id: row.filer_id,
             candidate_fullname: candidateName,
             office: _.startCase(_.lowerCase(row.office)),
@@ -65,17 +73,17 @@ module.exports = new Promise((resolve, reject) => {
           };
           filer_ids.push(row.filer_id);
           pdcCandidates.push(candidate);
-          // console.log(
-          //   '📇',
-          //   candidate.candidate_filer_id,
-          //   candidate.candidate_fullname
-          // );
+          console.log(
+            '📇',
+            candidate.candidate_filer_id,
+            candidate.candidate_fullname
+          );
         }
       }
       if (newCandidates.length > 0) {
-        console.log('### Add to load-config-names.json')
+        console.log('### Add to load-config-names.json\n')
         console.log(JSON.stringify(newCandidates, null, 2))
-        console.log('### Add to load-config-names.json')
+        console.log('\n### Add to load-config-names.json')
       }
       resolve(pdcCandidates);
     })
