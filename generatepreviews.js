11246f7c96d71e711d921f7813c2f5587844e79b
Adam Avenir <adamavenir@hey.com>
2023-10-21 02:31:14 +0000

    refine preview generator

--- generatepreviews.js
+++ generatepreviews.js
@@ -12,24 +12,34 @@ const jsonFiles = fs.readdirSync(dataDir).filter(file => file.endsWith('.json'))
 const slugs = jsonFiles.map(file => file.slice(5, -5));
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: 'new' }); // set headless mode to false
 
   for (const slug of slugs) {
     const page = await browser.newPage();
     // Set the viewport to emulate a high-density (Retina) display
     await page.setViewport({ width: 565, height: 333, deviceScaleFactor: 2 });
-    await page.goto(`http://localhost:8000/${slug}/preview`, { waitUntil: 'networkidle2' });
 
-    const element = await page.$('body'); // assuming the element to screenshot is body
-    const screenshot = await element.screenshot({
-      path: path.join(imageDir, `${slug}.png`),
-      clip: {
-        x: 0,
-        y: 0,
-        width: 565,
-        height: 333
-      }
-    });
+    try {
+      await page.goto(`http://localhost:8000/${slug}/preview`, { waitUntil: 'networkidle2', timeout: 10000 });
+      
+      // Wait for 5 seconds
+      await page.waitForTimeout(5000);
+
+      const element = await page.$('body'); // assuming the element to screenshot is body
+      const screenshot = await element.screenshot({
+        path: path.join(imageDir, `${slug}.png`),
+        clip: {
+          x: 0,
+          y: 0,
+          width: 565,
+          height: 333
+        }
+      });
+    } catch (error) {
+      console.log(`Failed to load: http://localhost:8000/${slug}/preview`);
+    } finally {
+      await page.close(); // close the page to free up resources
+    }
   }
 
   await browser.close();
