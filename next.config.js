d74cfe793150161b75e993b9567cae103e4c49f2
Adam Avenir <adam@avenir.party>
2025-09-18 23:40:31 -0700

    let netlify handle optimizing images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   // We'll start with these basic settings
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
